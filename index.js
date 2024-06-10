7ba5fede08516a14bc7b8860d893fd5c7183dc0d
as6500 <sofiapaisousa@gmail.com>
2024-06-10 22:03:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-// Dependencies of the project
+// Dependencies of the project 
+//hi 
 const express = require('express');
 const mysql = require("mysql2");
 const app = express(); // Create a new instance of express
